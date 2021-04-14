fa2857fcf5082b5023d643019c00637fe29b2b25
Thaithanh1 <77682579+Thaithanh1@users.noreply.github.com>
2021-04-14 13:52:45 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-//$url = parse_url(getenv("DATABASE_URL"));
-//$host = $url["host"];
-define('HOST', 'ec2-34-225-103-117.compute-1.amazonaws.com') ;
-define('PORT', '5432')
-define('USERNAME', 'eophfavabkrorx');
-define('PASSWORD', 'cada536917ba323da06136f1b0fd36d8eeae9fee194a8d611e249fd04c60fa78');
-define('DATABASE', 'dcg72tr1l83dd3');
