589a14a5c5ea2ed08b59d675fd32a6dbea735f36
Thaithanh1 <77682579+Thaithanh1@users.noreply.github.com>
2021-04-14 09:46:43 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,8 @@
 <?php
-$conn = pg_connect("host=ec2-34-225-103-117.compute-1.amazonaws.com port=5432 dbname=dcg72tr1l83dd3 user=eophfavabkrorx password=cada536917ba323da06136f1b0fd36d8eeae9fee194a8d611e249fd04c60fa78");
-if(!$conn){echo "Lost Connect";}
+//$url = parse_url(getenv("DATABASE_URL"));
+//$host = $url["host"];
+define('HOST', 'ec2-34-225-103-117.compute-1.amazonaws.com') ;
+define('PORT', '5432')
+define('USERNAME', 'eophfavabkrorx');
+define('PASSWORD', 'cada536917ba323da06136f1b0fd36d8eeae9fee194a8d611e249fd04c60fa78');
+define('DATABASE', 'dcg72tr1l83dd3');
